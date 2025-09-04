8d527c3cf3b6ef7d150f2502b335ac8443b67963
kajal mishra <111572290+mishrakajal2200@users.noreply.github.com>
2025-09-04 12:46:13 -0500

    form first

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true, 
+  },
 };
 
 export default nextConfig;
